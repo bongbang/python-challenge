e12563952919869d9d099f0781f6a825779acb2c
Tom Vamvanij <tom.vamvanij@gmail.com>
2025-10-27 20:27:02 +0700

    Simplify loop

--- 4.py
+++ 4.py
@@ -3,10 +3,9 @@ from helpers import load_html
 url_stub = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='
 
 for n in ['12345', '8022']:
-    while True:
+    while all([char.isdigit() for char in n]):
         string = load_html(url_stub + n)
         print(string)
         n = string.split()[-1]
-        if not all([char.isdigit() for char in n]):
-            print()
-            break
+    else:
+        print()
