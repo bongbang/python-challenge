384221d84d49a09f43669511510b345221ce6379
Tom Vamvanij <tom.vamvanij@gmail.com>
2025-10-28 07:04:52 +0700

    Tighten code and rationalize names

--- 4.py
+++ 4.py
@@ -2,10 +2,10 @@ from helpers import read_text
 
 url_stub = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='
 
-for number in ['12345', '8022']:
-    while all([char.isdigit() for char in number]):
-        string = read_text(url_stub + number)
-        print(string)
-        number = string.split()[-1]
+for nothing in ['12345', '8022']:
+    while all(char.isdigit() for char in nothing):
+        clue = read_text(url_stub + nothing)
+        print(clue)
+        nothing = clue.split()[-1]
     else:
         print()
