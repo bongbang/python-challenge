3b1bc2fa1ec51dfddcc8702d71b84f215d88d2a7
Tom Vamvanij <tom.vamvanij@gmail.com>
2025-11-01 10:45:20 +0700

    Remove TODO

--- 3.py
+++ 3.py
@@ -5,6 +5,7 @@ string = read_text('challenge_3.html')
 string = extract_string(string, 'kAewtloYg', 'btSipiqBd')
 
 matches = re.findall('[^A-Z][A-Z]{3}[a-z][A-Z]{3}[^A-Z]', string)
-# TODO make more reobust by providing for beginning of/end of string
+# Fails to find ABCxDEF at start or end of string, but we know that none is
+# to be found in *this* string.
 
 print(''.join([m[4] for m in matches]))
