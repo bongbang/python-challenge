3f6630bc5853441f264eb5a9bc4c4b4e184e1a66
Tom Vamvanij <tom.vamvanij@gmail.com>
2025-11-05 13:40:56 +0700

    Improve semantics

--- 4.py
+++ 4.py
@@ -2,7 +2,7 @@ from helpers import read_text
 
 url_stub = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='
 
-for nothing in ['12345', '8022']:
+for nothing in ('12345', '8022'):
     while all(char.isdigit() for char in nothing):
         clue = read_text(url_stub + nothing)
         print(clue)
